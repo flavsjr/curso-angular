c2e47c3336a2c8b1859449e14e36a68c6161a84c
flavsjr <flavianogjr@gmail.com>
2024-04-18 22:08:49 -0300

    Criando os componentes separadamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardPriceComponent } from './card-price/card-price.component';
+import { CardPlanComponent } from './card-plan/card-plan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardRoxoComponent
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardPriceComponent,
+    CardPlanComponent
   ],
   imports: [
     BrowserModule,
